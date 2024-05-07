eeecbe2e5275aa9a10f4b687e5f0bf9ddaf1b982
Euchupi <youngroosevelt@163.com>
2024-05-06 22:29:27 -0700

    CH1 calibration pythn

--- python/CH1_smooth_DC_calibration.py
+++ python/CH1_smooth_DC_calibration.py
@@ -6,14 +6,14 @@ import struct
 
 
 memory_file = os.open('/dev/mem',os.O_RDWR)
-mem = mmap.mmap(memory_file,length=mmap.PAGESIZE,offset=0x42000000)
+mem = mmap.mmap(memory_file,length=mmap.PAGESIZE,offset=0x42100000)
 
 N = 10000 # Samples 
 Data_array = np.zeros(N)
 
 for i in range(N):
-	CH1_raw = mem[0:2]
-	CH1 = struct.unpack('<h',CH1_raw)[0]
+	CH1_raw = mem[0:4]
+	CH1 = struct.unpack('<i',CH1_raw)[0]
 	Data_array[i] = CH1 
 	
 mem.close()
